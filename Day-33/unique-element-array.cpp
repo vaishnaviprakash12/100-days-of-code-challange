409dea6f5d7574f5a680f7a12e11dea141612067
Vaishnavi Prakash <98525625+vaishnaviprakash12@users.noreply.github.com>
2023-06-13 23:49:36 +0530

    Add files via upload

---
+++ Day-33/unique-element-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int sum=0;
+
+        for(int i=0;i<nums.size();i++){
+
+            if(count(nums.begin(),nums.end(),nums[i])==1){
+                sum+=nums[i];
+            }
+        }
+        return sum;
+    }
+};
