49f8842bd1cd88763601344eaaea582cf7c24f8d
Vaishnavi Prakash <98525625+vaishnaviprakash12@users.noreply.github.com>
2023-06-20 20:59:51 +0530

    Add files via upload

---
+++ Day-40/single-numberII.cpp
@@ -0,0 +1,15 @@
+//137. Single Number II
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones=0;
+        int twos=0;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+            ones=(ones^nums[i])&~twos;
+            twos=(twos^nums[i])&~ones;
+        }
+        return ones;
+    }
+};
