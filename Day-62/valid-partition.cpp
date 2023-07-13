264eed7c9fe8077661f35086c4e4677748ad3e7a
Vaishnavi Prakash <98525625+vaishnaviprakash12@users.noreply.github.com>
2023-07-13 08:21:27 +0530

    Add files via upload

---
+++ Day-62/valid-partition.cpp
@@ -0,0 +1,13 @@
+//2740. Find the Value of the Partition
+class Solution {
+public:
+    int findValueOfPartition(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int ans = nums[n-1];
+        for(int i = 1; i < n; i++){
+            ans = min(ans,nums[i]-nums[i-1]);
+        }
+        return ans;
+    }
+};
