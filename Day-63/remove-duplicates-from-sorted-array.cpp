ea4b89aa98020b718ad6d7dabbb2e60917490308
Vaishnavi Prakash <98525625+vaishnaviprakash12@users.noreply.github.com>
2023-07-14 22:33:49 +0530

    Add files via upload

---
+++ Day-63/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,13 @@
+//80. Remove Duplicates from Sorted Array II
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+       int i=0;
+       for(int n:nums){
+           if(i<2||n>nums[i-2]){
+               nums[i++]=n;
+           }
+       }
+       return i;
+    }
+};
