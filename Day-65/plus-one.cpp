4429ab0a26624630465b3ff06c2b4e12b80baaac
Vaishnavi Prakash <98525625+vaishnaviprakash12@users.noreply.github.com>
2023-07-16 20:03:43 +0530

    Add files via upload

---
+++ Day-65/plus-one.cpp
@@ -0,0 +1,17 @@
+//66. Plus One
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;i--){
+            if(digits[i]!=9){
+                digits[i]=digits[i]+1;
+                break;
+            }
+            else { digits[i]=0;}
+        }
+        if(digits[0]==0){
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
+    }
+};
