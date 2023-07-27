7d72a2047e2d9a38c60903e57f22f957661a9e25
Vaishnavi Prakash <98525625+vaishnaviprakash12@users.noreply.github.com>
2023-07-27 23:44:27 +0530

    Add files via upload

---
+++ Day-76/valid-anagram.cpp
@@ -0,0 +1,11 @@
+//242. Valid Anagram
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       sort(s.begin(),s.end());
+       sort(t.begin(),t.end());
+
+       return s==t;
+
+    }
+};
